4a4e7531c0028b5e44e12e1eedfd0bb69c13262f
Houjun Liu <hliu@shabang.cf>
2021-10-10 22:43:22 -0700

    Working everything. I am done for the night

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,80 @@
-mod table;
 mod utils;
+mod table;
+use std::num::ParseIntError;
 
-pub use table::Rainbow;
-use clap;
-use clap::{Arg, App, SubCommand};
+use std::fs;
 
-fn main() {
-    let menu = clap_app!(
+#[macro_use]
+extern crate clap;
 
-
-
-    // println!("{:?}", base64::encode([1,2,3,4,5,6,7]).as_bytes());
-
-    // "abc" = "900150983cd24fb0d6963f7d28e17f72"
-
-    // let res:[u8;16] = md5::compute("hewoo").into();
-    // println!("{}", utils::reduce(&res, 23).iter().map(|i| *i as char).collect::<String>());
-
-    let re:Vec<&str>;
-    if let Ok(file) = fs::read_to_string("./10-million-password-list-top-1000000.txt") {
-        re = file.lines().collect();
-        let res = table::Rainbow::create(999998, 10000, 5, Some(&re));
-        if let Ok(table) = res {
-            if let Ok(_) = table.write_json("./pwl.json") {} else {};
-            // let mut v = vec![];
-            // let mut counter = 0;
-            // for row in &table.rainbow_table {
-            //     if v.contains(&row.1) {
-            //         counter +=1;
-            //     }
-            //     v.push(row.1.clone());
-            // }
-            // println!("Duplicated rows: {}", counter);
-
-            // let r:[u8;16] = md5::compute("hne3to").into();
-            // println!("\n \n result: {:?}", table.decode(&r, 200));
-        }
-
-    }
-
-    // println!("table: {:?}", res);
+/*
+ * Hello, gentle Reader!
+ * This file contains the driver code to the CLI of `rainbow`, and is
+ * likely not helpful to the usage of this package as a library. Probing
+ * `lib.rs` is likely more helpful to that end.
+ *
+ */
 
 
-    // let bar = ProgressBar::new(10000);
-    // println!("{:?}", utils::generate_chain("ha", 10000, Some(&bar)));
-    // println!("{:?}", utils::generate_chain("hallo", 10000, None));
+// Decode a hexdecimal string
+// https://stackoverflow.com/questions/52987181/how-can-i-convert-a-hex-string-to-a-u8-slice
+fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
 }
 
-// 4294967295
+fn main() {
+    let menu = clap_app!(rainbow =>
+                         (version: "0.0.1")
+                         (author: "Houjun Liu <houliu@nuevaschool.org>")
+                         (about: "Badly implemented MD5 Rainbow table")
+                         (@subcommand attack =>
+                          (about: "Use a rainbow table to crack a password")
+                          (@arg TABLE: +required "Input JSON file path for a created rainbow table")
+                          (@arg DIGEST: +required "Lower-hex digest of a md5 hash to crack")
+                          (@arg THREADS: -t --threads +takes_value "Number of thread workers")
+                         )
+                         (@subcommand create =>
+                          (about: "Create a rainbow table")
+                          (@arg OUTPUT: +required "Output JSON file path for the created rainbow table")
+                          (@arg PASSWORDFILE: -p --pfile +takes_value "Newline-seperated password file path")
+                          (@arg THREADS: -t --threads +takes_value "Number of thread workers")
+                          (@arg LENGTH: -l --length +takes_value "The length of rainbow chains")
+                          (@arg SAMPLES: -s --samples +takes_value "The number of samples to seed")
+                         )
+    ).get_matches();
+
+    if let Some(menu) = menu.subcommand_matches("attack") {
+        let table:&str = menu.value_of("TABLE").unwrap();
+        let digest:Vec<u8> = decode_hex(menu.value_of("DIGEST").unwrap()).unwrap();
+        let threads:u8 = menu.value_of("threads").unwrap_or("10").parse::<u8>().unwrap();
+
+        let table:Rainbow = Rainbow::read_json(table).unwrap();
+
+        if let Ok(answer) = table.decode(&digest, threads) {
+            println!("{}", answer);
+            std::process::exit(0);
+        } else if let Err(err) = table.decode(&digest, threads) {
+            println!("Error! {}", err);
+            std::process::exit(1);
+        }
+    } else if let Some(menu) = menu.subcommand_matches("create") {
+        let output:&str = menu.value_of("OUTPUT").unwrap();
+        let threads:u8 = menu.value_of("THREADS").unwrap_or("10").parse::<u8>().unwrap();
+        let samples:u32 = menu.value_of("SAMPLES").unwrap_or("50000").parse::<u32>().unwrap();
+        let length:u32 = menu.value_of("LENGTH").unwrap_or("4000").parse::<u32>().unwrap();
+        if let Some(passwordfile) = menu.value_of("PASSWORDFILE") {
+            let re:Vec<&str>;
+            if let Ok(file) = fs::read_to_string(passwordfile) {
+                re = file.lines().collect();
+                let table = table::Rainbow::create(re.len() as u32, length, threads, Some(&re)).unwrap();
+                if let Ok(_) = table.write_json(output) {} else {};
+            }
+        } else {
+            let table = table::Rainbow::create(samples, length, threads, None).unwrap();
+            if let Ok(_) = table.write_json(output) {} else {};
+        }
+    }
+}
